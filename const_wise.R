74e678994b4ddf76ebf5c212480d7bc62fb3af40
kuberiitb <kuberbhu@gmail.com>
2014-06-19 01:40:07 +0530

    Update-Add winner variable

--- const_wise.R
+++ const_wise.R
@@ -1,90 +1,73 @@
-###############	Code to crawl Constituencywise data from http://eciresults.nic.in		###############
+###############  Code to crawl Constituencywise data from http://eciresults.nic.in		###############
 
 library(XML)
 library(stringr)
 library(plyr)
-setwd("D:/")
 
-full_data<- full_data[which(is.na(full_data$State)),]
-names(full_data)
-
-for(stateup in 1:2){
-	if(stateup==1){
-		su="S"
-		count=29
-	}else
-	{
-		su="U"
-		count=7
-	}
-
-	for(i in 1:count){
-		#i=1
-		#su="U"
-
-		if(i <10){
-			url=paste0("http://eciresults.nic.in/statewise",su,"0",i,".htm")
-		}else
-		{
-			url=paste0("http://eciresults.nic.in/statewise",su,i,".htm")
-		}
-		print(url)
-		tables <- readHTMLTable(url)
-		d<-data.frame(tables[[1]])
-		d<-d[(!is.na(d$V8) & d$V1!="Constituency"),]
-		nconst=nrow(d)
-		state=gsub(" Result Status","",data.frame(tables[[1]])[14,1])
-		print(state)
-		for(j in 1:nconst){
-			#j=1
-			if(i <10){
-				url=paste0("http://eciresults.nic.in/Constituencywise",su,"0",i,j,".htm")
-			}else{
-			url=paste0("http://eciresults.nic.in/Constituencywise",su,i,j,".htm")
-			}
-			print(url)
-			#Crawling data from each Constituency
-			#
-			#
-			#	url=paste0("http://eciresults.nic.in/ConstituencywiseU021.htm")
-			tables <- readHTMLTable(url)
-			data<-data.frame(tables[[1]])
-			data<-data[(!is.na(data$V2) & !(data$V2 %in% c("Party",""))),c(1:3)]
-			droplevels(data)
-			names(data)<-c("Candidate","Party","Votes")
-			#summary(data)
-			data$State=state
-			data$Const=factor(data.frame(tables[[1]])[17,1])
-			#full_data<-rbind(all_data,data)
-			if(exists(x="full_data")){
-				full_data<-rbind(full_data,data)
-			}else
-			{
-				assign("full_data",data,envir=in.env())
-			}
-			#print(full_data)
-			dim(full_data)
-		}
-
-	}
+DataPull<- function(su="S", id){
+  for(i in id){
+    url=paste0("http://eciresults.nic.in/statewise",su,sprintf("%02d", i),".htm")
+    print(url)
+    
+    tables <- readHTMLTable(url)
+    d<-data.frame(tables[[1]])
+    d<-d[(!is.na(d$V8) & d$V1!="Constituency"),]
+    nConst=nrow(d)
+    state=gsub(" Result Status","",data.frame(tables[[1]])[14,1])
+    print(state)
+    for(j in 1:nConst){
+      #j=1
+      url=paste0("http://eciresults.nic.in/Constituencywise",su,sprintf("%02d", i),j,".htm")
+      print(url)
+      
+      #Crawling data from each Constituency    
+      tables <- readHTMLTable(url)
+      data<-data.frame(tables[[1]])
+      data<-data[(!is.na(data$V2) & !(data$V2 %in% c("Party",""))),c(1:3)]
+      droplevels(data)
+      names(data)<-c("candidate","party","votes")
+      #summary(data)
+      data$State=state
+      data$Const=factor(data.frame(tables[[1]])[17,1])
+      #electionData<-rbind(all_data,data)
+      if(exists(x="electionData")){
+        electionData<-rbind(electionData,data)
+      }else
+      {
+        electionData=data
+      }
+      #print(electionData)
+      dim(electionData)
+    }
+    
+  }
+  return(electionData)
 }
 
-full_data$winner=0
-full_data[1,"winner"]=1
-full_data[1:100,c(1,2,3,6)]
-for(i in 2:nrow(full_data)){
-  if(full_data[i-1,"Votes"] < full_data[i,"Votes"]){
-    full_data[i,"winner"]=1
+electionData=DataPull(su="U", id=1:7)
+dim(electionData)
+electionData=DataPull(su="S", id=1:5)
+electionData=DataPull(su="S", id=6:28)
+
+electionData<-electionData[!duplicated(electionData),]
+electionData$votes<-as.numeric(as.character(electionData$votes))
+
+electionData= electionData[order(electionData$State, electionData$Const,-electionData$votes),]
+head(electionData)
+electionData$const=gsub("^[^-]+-\\s*", "",electionData$Const)
+electionData$Const=NULL
+
+electionData$winner=0
+electionData[1,"winner"]=1
+for(i in 2:nrow(electionData)){
+  if(electionData[i-1,"votes"] < electionData[i,"votes"]){
+    electionData[i,"winner"]=1
   }
 }
 
-full_data
-names(full_data)
-unique(full_data$State)
-dim(full_data)
-unique(duplicated(full_data))
+names(electionData)
+unique(electionData$State)
+unique(electionData$const)
+dim(electionData)
 
-full_data<-full_data[!duplicated(full_data),]
-full_data$Votes<-as.numeric(full_data$Votes)
-write.csv(full_data,"const_wise_election_result_2014.csv",row.names=FALSE)
-
+write.csv(electionData,"const_wise_election_result_2014.csv",row.names=FALSE)
